3405fd670dd2ee46c2497d64862ea05fb2ce37d6
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-02-21 17:49:33 +0100

    end

--- resources/views/trains/show.blade.php
+++ resources/views/trains/show.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="container">
   <div class="row">
-    <div class="col">
-      <h1>{{ $train->azienda }}</h1>
-    </div>
-  </div>
-  <div class="row">
       <div class="col">
         <div class="card mb-3">
           <div class="card-body text-center">
-            <h1 class="card-title">{{ $train->azienda }}</h1>
-            <p class="card-text">{{ $train->stazione_di_partenza }}</p>
-            <p class="card-text">{{ $train->stazione_di_arrivo }}</p>
+            <h5 class="card-title">Azienda: {{ $train->azienda }}</h5>
+            <h5>Codice Treno: {{ $train->codice_treno }}</h2>
+            <p class="card-text">Partenza: {{ $train->stazione_di_partenza }}</p>
+            <p>Orario: {{ $train->orario_di_partenza }}</p>
+            <p class="card-text">Arrivo: {{ $train->stazione_di_arrivo }}</p>
+            <p>Orario: {{ $train->orario_di_arrivo }}</p>
+            <h4>numero carrozze: {{ $train->numero_carrozze }}</h4>
+            <a href="{{ route('trains.index', $train) }}" class="btn btn-primary">View Trains</a>
           </div>
         </div>
       </div>
